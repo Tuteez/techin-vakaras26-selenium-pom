d213aef26625094391ee0699155dfec5307e4bed
maksim.trojan <maksimastrojan@gmail.com>
2026-04-02 15:44:44 +0300

    init

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+//        turn on headless
+//        options.addArguments("--headless=new");
+        options.setAcceptInsecureCerts(true);
+        driver = new ChromeDriver(options);
+        driver.get("https://opencart.abstracta.us/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
